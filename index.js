2e31fdf1764cdd6f9344115f245d41fc0dccfbbe
Cheikh Bassirou GUEYE <bagueye905@gmail.com>
2025-07-13 08:06:01 +0000

    reading all params

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.get('/api/courses', (req, res) => {
 })
 
 //defining a route to get a specific course with its id
-app.get('/api/courses/:id', (req, res) => {
+app.get('/api/courses/:year/:month', (req, res) => {
     //to read the parameter id, we use the req object
-    res.send(req.params.id)
+    res.send(req.params)
 })
 
 //PORT is an environment variable which tell on wich port a process run. that what we gonna use here instead of fixing the port with the value 3000
