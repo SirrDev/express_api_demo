5ab4a8b966edddb238bea773ef01d700f4cb0f04
Cheikh Bassirou GUEYE <bagueye905@gmail.com>
2025-07-13 07:38:11 +0000

    implementing routes or endspoints

---
+++ index.js
@@ -0,0 +1,17 @@
+//loading the express module wich return a function named expres
+const express = require("express")
+//call of the function that return an object of type express, by convention that object is called app
+//the object app got a bunch of useful methods like get(), put(), post(), delete()
+const app = express()
+
+/*implementing endpoints that respond to http get request, that methods gonna get two arguments: the path or the url and the callback method wich is the function that we gonna call when we have an http request*/
+app.get('/', (req, res) => {
+    /*the object req above has a bunch f methods that give us information about the incoming request. the res object define the response given after a http request*/
+    res.send('Hello world')
+})
+
+app.get('/api/courses', (req, res) => {
+    res.send([1,2,3])
+})
+
+app.listen(3000, () => {console.log('Listening on port 3000...')})
