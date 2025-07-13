5bed4cce7fa4dd63527ae43ba05e78d9b812a0ab
Cheikh Bassirou GUEYE <bagueye905@gmail.com>
2025-07-13 08:30:47 +0000

    handling http get request

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const express = require("express")
 //the object app got a bunch of useful methods like get(), put(), post(), delete()
 const app = express()
 
+const courses = [
+    {id: 1, name: "course 1"},
+    {id: 2, name: "course 2"},
+    {id: 3, name: "course 3"},
+]
+
 /*implementing endpoints that respond to http get request, that methods gonna get two arguments: the path or the url and the callback method wich is the function that we gonna call when we have an http request*/
 app.get('/', (req, res) => {
     /*the object req above has a bunch f methods that give us information about the incoming request. the res object define the response given after a http request*/
@@ -11,14 +17,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3])
+    res.send(courses)
 })
 
 //defining a route to get a specific course with its id
-app.get('/api/courses/:year/:month', (req, res) => {
+app.get('/api/courses/:id', (req, res) => {
     //to read the parameter id, we use the req object
     //using query string parameters with query, it's for optional parameter that are not essential # raw params, ex:?sortedBy=name
-    res.send(req.query)
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if(!course) res.status(404).send('The course with the given ID was not found.')
+    res.send(course)
+
 })
 
 //PORT is an environment variable which tell on wich port a process run. that what we gonna use here instead of fixing the port with the value 3000
